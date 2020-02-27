9af9746ad456d2d401558df7999a95359da3f56f
Shane Sloan <sloan.shane.1991@gmail.com>
2020-02-27 16:57:36 -0600

    updated formatting on expense list item, added new library to package.json

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import numeral from 'numeral';
 import {Link} from 'react-router-dom';
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
@@ -8,7 +9,7 @@ const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
       <p>Expense: {description}</p>
     </Link>
     <ul>
-      <li>Amount: {amount}</li>
+      <li>Amount: {numeral(amount/100).format('$0,0.00')}</li>
       <li>Date Created: {moment(createdAt).format("MM/DD/YYYY")}</li>
     </ul>
   </div>
