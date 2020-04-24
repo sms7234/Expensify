59e4de03572eb90f152b7ee519f212a94fe13d8a
Shane Sloan <sloan.shane.1991@gmail.com>
2020-04-24 17:28:48 -0500

    Added line graph that displays multiple bank accounts

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Chart from 'react-google-charts';
+import {Doughnut, Line} from 'react-chartjs-2';
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses'
 import DashboardSummary from '../independents/DashboardSummary';
 import ExpenseListFilters from '../filters/ExpenseListFilters';
 
-export const DashboardPage = ({income, expenses, pieData, lineData}) => (
+export const DashboardPage = ({income, expenses, pieData, lineData, donutData}) => (
   <div>
     <DashboardSummary income={income} expenses={expenses} />
     <div className="content-container">
       <ExpenseListFilters />
       <div className="content-container--charts">
         <h1> Purchases by Category </h1>
-        <Chart
-          chartType="PieChart"
-          height={400}
-          loader={<div>Loading Chart</div>}
-          data={pieData}
-          options={{
-            is3D: true,
-          }}
-          rootProps={{ 'data-testid': '2' }}
+        <Doughnut
+          data={donutData}
+        />
+        <Line
+          data={lineData}
         />
         <h1> Remaining Funds (in $) by Date </h1>
-        <Chart
+        {/* <Chart
           chartType="LineChart"
           height={400}
           loader={<div>Loading Chart</div>}
@@ -41,13 +38,28 @@ export const DashboardPage = ({income, expenses, pieData, lineData}) => (
             },
           }}
           rootProps={{ 'data-testid': '1' }}
-        />
+        /> */}
       </div>
     </div>
   </div>
 )
 
 const mapStateToProps=(state)=>{
+  //color array
+  const colors = ['#E74C3C','#9B59B6', '#2980B9', '#1ABC9C', '#27AE60', '#F1C40F', '#F39C12', '#D35400', '#C0392B', '#8E44AD', '#3498DB', '#16A085', '#2ECC71', '#F5B041', '#EB984E' ];
+
+  //assign colors - pass in array with desired length
+  const colorAssign = (desiredQty) => {
+    const max = colors.length;
+    const holder = [];
+    const n=0;
+    desiredQty.forEach((item,index) => {
+      if (index === max*(n+1)){n = n+1}
+      holder.push(colors[index-(n*max)])
+    })
+    return holder;
+  }
+
   //Sum data
   const sumItems = (arr) => {
     let holder = 0;
@@ -68,24 +80,91 @@ const mapStateToProps=(state)=>{
     return arr.filter((item)=> item.category!=="Income")
   };
 
-  const filterPie = (arr) => {
-    const tempHolder = [];
-    const pieData = [['Category', 'Cost']];
+  const FilterDoughnut = (arr) => {
+    const labelHolder = [];
+    const dataHolder = [];
     arr.forEach((item) => {
-      if(!tempHolder.includes(item.category) && item.category!=="Income") {
-        tempHolder.push(item.category)
+      if(!labelHolder.includes(item.category) && item.category!=="Income") {
+        labelHolder.push(item.category)
       }
     });
-    tempHolder.forEach((item) => {
+    //assign colors
+    const colorHolder = colorAssign(labelHolder);
+
+    labelHolder.forEach((item) => {
       let sum = 0;
       arr.forEach((subItem,index) => {
         if(subItem.category===item) {
           sum = sum + subItem.amount;
         }
       });
-      pieData.push([item, sum/100]);
+      dataHolder.push(sum/100);
+    });
+    const donutData = {datasets:[{data: dataHolder, backgroundColor: colorHolder}], labels:labelHolder}
+    return donutData;
+  };
+
+  const filterLineData = (arr) => {
+    //holders
+    const accounts = {};
+    const labels= [];
+    let counter = 0
+    //sort by date
+    const originalData = arr.sort((a,b) => {
+      return a.purchaseDate < b.purchaseDate ? -1: 1;
+    });
+    //pull out unique dates & accounts
+    originalData.forEach((item) => {
+      const curDate = moment(item.purchaseDate).format('MM/DD/YY');
+      if (!labels.includes(curDate)){
+        labels.push(curDate)
+      }
+      if(!accounts.hasOwnProperty(item.account)) {
+        accounts[item.account]=[0];
+      }
     })
-    return pieData;
+    //run through array & sort out expenses into acctObj
+    let lastMatch = 0;
+    const len = originalData.length;
+    const accountList = Object.keys(accounts);
+    labels.forEach((item,index) => {
+      //add prev date sum (in accounts array) as the next item in array
+      if(index>0){
+        accountList.forEach((item) => {
+          accounts[item].push(accounts[item][index-1]);
+        })
+      }
+      for(let i=lastMatch; i<len; i++){
+        //dates are the same...
+        if(moment(item).isSame(originalData[i].purchaseDate, 'day')) {
+          const acctName = originalData[i].account;
+          if(originalData[i].category === 'Income'){
+            accounts[acctName][index] += originalData[i].amount/100
+          } else {
+            accounts[acctName][index] -= originalData[i].amount/100
+          }
+          lastMatch = i;
+          counter +=1;
+        }
+        //item date is greater than label date
+        if(moment(originalData[i].purchaseDate).isAfter(item, 'day')) {
+          break;
+        }
+      }
+    })
+
+    //assign colors
+    const colorHolder = colorAssign(accountList);
+
+    //reformat data for chart
+    const datasets = [];
+    accountList.forEach((item, index) => datasets.push({label: item, data: accounts[item], backgroundColor: colorHolder[index], borderColor: colorHolder[index], fill: false}))
+    return {labels, datasets}
+
+    //return correctly formatted information
+    console.log('labels: ',labels, 'datasets: ',datasets)
+    console.log('number of matches: ', counter, 'total expenses: ', originalData.length)
+
   }
 
   const filterLine = (arr) => {
@@ -136,8 +215,8 @@ const mapStateToProps=(state)=>{
     a: selectExpenses(state.expenses, state.filters),
     expenses: sumItems(filterExpenses(visibleExpenses)),
     income: sumItems(filterIncome(visibleExpenses)),
-    pieData: filterPie(selectExpenses(state.expenses, state.filters)),
-    lineData: filterLine(selectExpenses(state.expenses, state.filters))
+    lineData: filterLineData(selectExpenses(state.expenses, state.filters)),
+    donutData: FilterDoughnut(selectExpenses(state.expenses, state.filters))
   };
 };
 
