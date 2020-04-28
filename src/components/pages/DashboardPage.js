25e4c50c03b4f329ff9b08b40e7c4b09f924ec31
Shane Sloan <sloan.shane.1991@gmail.com>
2020-04-28 18:42:08 -0500

    Added a new chart to compare history of category expenses (per month) in specific categories

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Doughnut, Line} from 'react-chartjs-2';
+import {Doughnut, Line, Bar} from 'react-chartjs-2';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import Select from 'react-select';
 import moment from 'moment';
 import selectExpenses from '../../selectors/expenses'
 import DashboardSummary from '../independents/DashboardSummary';
@@ -12,12 +13,46 @@ export class DashboardPage extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      legends: false
+      legends: false,
+      labels: ['no data'],
+      categoryData: [{data: [0]}]
+      }
     };
-  }
   onToggle = () => {
     (this.state.legends) ? this.setState({legends:false}) : this.setState({legends:true});
   }
+  onCategorySelect = (opt) => {
+    //sort input into array & set state
+    const chosenCategories = [];
+    const datasets = [];
+    let n=0;
+
+    //colors for charts
+    const colors = ['#E74C3C','#9B59B6', '#2980B9', '#1ABC9C', '#27AE60', '#F1C40F', '#F39C12', '#D35400', '#C0392B', '#8E44AD', '#3498DB', '#16A085', '#2ECC71', '#F5B041', '#EB984E' ];
+    const max = colors.length;
+
+    opt.forEach((item) => {chosenCategories.push(item.value)});
+
+    //run through categoryData
+    const months = Object.keys(this.props.categoryData);
+    chosenCategories.forEach((item, index) => {
+      const data = [];
+      const label = item;
+      //loop through each date & gather category data
+      months.forEach((subItem) => {
+        data.push(this.props.categoryData[subItem][item])
+      });
+      //assign colors
+      if(index===max){n+=1}
+      const backgroundColor = colors[index-(n*max)];
+      //push data to final holders
+      datasets.push({label, data, backgroundColor})
+    })
+    // console.log('labels: ', months, ' categoryData: ', datasets)
+    this.setState({labels: months})
+    this.setState({categoryData: datasets})
+  }
+
   render() {
     return (
       <div>
@@ -41,6 +76,30 @@ export class DashboardPage extends React.Component{
                 <div className="content-container--charts">
                   <h1> Remaining Funds (in $) by Date </h1>
                   <Line data={this.props.lineData} options={{legend: {display:this.state.legends}}} />
+                </div>
+              </Tab>
+              <Tab eventKey="bar" title="Income vs Expense">
+                <div className="content-container--charts">
+                  <h1> Monthly Summary </h1>
+                  <Bar data={this.props.monthData} options={{legend: {display:this.state.legends}}} />
+                </div>
+              </Tab>
+              <Tab eventKey="variable-bar" title="Category History">
+                <div className="content-container--charts">
+                  <h1> Category History </h1>
+                  <h3>add/remove desired categories</h3>
+                  <p>***Warning*** Information only updates when categories are change</p>
+                  <p>There is no auto-update when dates change</p>
+                  <Select
+                    placeholder="Select Categories to show"
+                    className="dropdown"
+                    isClearable
+                    isMulti
+                    options={this.props.categoryList}
+                    onChange={this.onCategorySelect}
+                  />
+                  <p>Chart HERE </p>
+                  <Bar data={{labels: this.state.labels, datasets: this.state.categoryData}} options={{legend: {display:this.state.legends}}} />
                 </div>
               </Tab>
             </Tabs>
@@ -87,9 +146,23 @@ const mapStateToProps=(state)=>{
     return arr.filter((item)=> item.category!=="Income")
   };
 
+  //filters categories into object for select component
+  const filterCategories = (categories) => {
+    const categoryHolder = [];
+    const categoryObj = []
+    categories.forEach((item) => {
+      categoryHolder.push(item.category);
+    })
+    const categoryList = Array.from(new Set(categoryHolder));
+    categoryList.forEach((item) => {
+      categoryObj.push({label: item, value: item})
+    })
+    return categoryObj;
+  }
+
+  //data for pie chart
   const FilterDoughnut = (arr) => {
     const labelHolder = {};
-
     //generate labels
     arr.forEach((item) => {
       if(!labelHolder.hasOwnProperty(item.category) && item.category!=="Income") {
@@ -110,7 +183,63 @@ const mapStateToProps=(state)=>{
     return donutData;
   };
 
-  const filterLineData = (arr) => {
+  //data for the expense vs income chart
+  const filterMonthlySums = (arr) => {
+    const monthSums = {};
+    const expenses=[];
+    const income=[];
+
+    //run through array & sort sums into month keys
+    arr.forEach((item) => {
+      //convert date to MM/YY
+      const date = moment(item.purchaseDate).format('MM/YY')
+      //check if month-key exists ?
+      if(!monthSums.hasOwnProperty(date)){
+        //create key with 0 as value
+        const obj = {Income: 0, Expenses: 0}
+        monthSums[date]=obj;
+      }
+
+      if(item.category === 'Income') {
+        monthSums[date]['Income'] += item.amount/100;
+      } else {
+        monthSums[date]['Expenses'] += item.amount/100
+      }
+    });
+
+    //reformat data
+    const labels = Object.keys(monthSums);
+    labels.forEach((item) => {
+      expenses.push(monthSums[item]['Expenses']);
+      income.push(monthSums[item]['Income']);
+    })
+    const datasets=[{
+      label: 'Expenses',
+      data: expenses,
+      type: 'bar',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+		  borderColor: 'rgba(255,99,132,1)',
+		  borderWidth: 1,
+		  hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+		  hoverBorderColor: 'rgba(255,99,132,1)'
+    }, {
+      label: 'Income',
+      data: income,
+      type: 'bar',
+      backgroundColor: 'rgba(24,255,100,0.2)',
+		  borderColor: 'rgba(24,255,100,1)',
+		  borderWidth: 1,
+		  hoverBackgroundColor: 'rgba(24,255,100,0.4)',
+		  hoverBorderColor: 'rgba(24,255,100,1)'
+    }];
+
+    const masterData = {labels, datasets}
+
+    return masterData;
+  }
+
+  //data for account chart
+  const filterLineData = (arr) => {arr
     //holders
     const accounts = {};
     const labels= [];
@@ -129,7 +258,7 @@ const mapStateToProps=(state)=>{
         accounts[item.account]=[0];
       }
     })
-    //run through array & sort out expenses into acctObj
+    //run through array & sort out expenses into acctObjarr
     let lastMatch = 0;
     const len = originalData.length;
     const accountList = Object.keys(accounts);
@@ -166,11 +295,45 @@ const mapStateToProps=(state)=>{
     const datasets = [];
     accountList.forEach((item, index) => datasets.push({label: item, data: accounts[item], backgroundColor: colorHolder[index], borderColor: colorHolder[index], fill: false}))
     return {labels, datasets}
+  }
 
-    //return correctly formatted information
-    console.log('labels: ',labels, 'datasets: ',datasets)
-    console.log('number of matches: ', counter, 'total expenses: ', originalData.length)
+  //data for category summary chart
+  const sumCategories = (expenses, categories) => {
+    //holders
+    const labels = [];
+    const categoryObj = {};
+    const masterData = {};
 
+    //create a unique categories array
+    const categoryHolder = []
+    categories.forEach((item) => {
+      categoryHolder.push(item.category);
+    })
+    const categoryList = Array.from(new Set(categoryHolder));
+    categoryList.forEach((item) => {categoryObj[item] = 0})
+
+    //run through array
+    expenses.forEach((item) => {
+      //convert dates
+      const date = moment(item.purchaseDate).format("MM/YY");
+
+      //assort into object
+      if(!masterData.hasOwnProperty(date)){
+        labels.push(date);
+        masterData[date] = {...categoryObj}
+
+      }
+
+      if(masterData[date].hasOwnProperty(item.category)) {
+        masterData[date][item.category] += item.amount/100;
+      } else {
+        //this means that a category input is incorrect
+        console.log(`An expense with purchased on ${moment(item.purchaseDate).format('MM/DD/YY')} has the category ${item.category} assigned, which does not reflect an accurate category type.  Please correct this information in order for accurate data for be presented in the graphs.`)
+      }
+    })
+    //returns data:{month1: {cat1: #, cat2: #}}
+    console.log(masterData);
+    return masterData;
   }
 
   // actual data
@@ -181,7 +344,11 @@ const mapStateToProps=(state)=>{
     expenses: sumItems(filterExpenses(visibleExpenses)),
     income: sumItems(filterIncome(visibleExpenses)),
     lineData: filterLineData(selectExpenses(state.expenses, state.filters)),
-    donutData: FilterDoughnut(selectExpenses(state.expenses, state.filters))
+    donutData: FilterDoughnut(selectExpenses(state.expenses, state.filters)),
+    monthData: filterMonthlySums(selectExpenses(state.expenses, state.filters)),
+    categoryData: sumCategories(selectExpenses(state.expenses, state.filters), state.categories),
+    categoryList: filterCategories(state.categories)
+
   };
 };
 
